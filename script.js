a9ba0918e5e38a0a15b2d4f67729cd1e24f80ee4
MintNiphada <niphada.y@kkumail.com>
2026-01-28 18:24:29 +0700

    add light and dark mode

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const backToTop = document.getElementById("backToTop");
+const toggleBtn = document.getElementById("themeToggle");
+const savedTheme = localStorage.getItem("theme");
 
+//arrow up
 window.addEventListener("scroll", () => {
   if (window.scrollY > 300) {
     backToTop.style.display = "block";
@@ -14,3 +17,26 @@ backToTop.addEventListener("click", () => {
     behavior: "smooth"
   });
 });
+
+//mode light dark
+if (savedTheme) {
+  document.body.classList.toggle("dark", savedTheme === "dark");
+} else {
+  const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  document.body.classList.toggle("dark", prefersDark);
+}
+
+updateIcon();
+
+toggleBtn.addEventListener("click", () => {
+  const isDark = document.body.classList.toggle("dark");
+  localStorage.setItem("theme", isDark ? "dark" : "light");
+  updateIcon();
+});
+
+function updateIcon() {
+  toggleBtn.innerHTML = document.body.classList.contains("dark")
+    ? '<i data-lucide="sun"></i>'
+    : '<i data-lucide="moon"></i>';
+  lucide.createIcons();
+}
